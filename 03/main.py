cdae250c82d9de487693c71ee5f641b57b8fa4a0
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2022-12-29 15:59:02 +0900

    Create main.py

---
+++ 03/main.py
@@ -0,0 +1,25 @@
+a,b,c,d,e,f
+true
+false
+false 
+true
+true
+false
+true
+true
+true
+false
+true
+true
+true
+코딩
+1
+2
+3
+4
+5
+user = input("숫자가 무엇인가요?")
+if 100:
+    print("짝수")
+else:
+    print("홀수")
