cc13234abe4d578f1592a0fc2f7a67aedafd72f8
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-01-05 16:02:07 +0900

    Create main.py

---
+++ 04/main.py
@@ -0,0 +1,33 @@
+x = int(input(나이를 입력하세요 : ))
+
+if age:
+  >= 10  and < 20:
+     print('10대')
+elif age:
+  >= 20 and < 30:
+     print('20대')
+elif age:
+  >= 30 and < 40:
+     print('30대')
+elif age:
+  10<:
+     print('10대 미만')
+else age:
+     print('40대 이상입니다')
+
+10대
+count = int(input('반복할 횟수를 입력하세요: '))
+ 
+i = 0
+while i = i+1:
+    print('Hello, world!', i)
+    
+i = 0
+result_even = 0
+ 
+while  i = i + 1:
+   if @@@:
+        result_even = result_even + i
+
+    
+
