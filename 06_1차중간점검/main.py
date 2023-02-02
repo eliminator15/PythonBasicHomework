0a120beafd5404d51b402990b436e7ad0831a681
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-02-02 17:27:01 +0900

    Update main.py

--- 06_1차중간점검/main.py
+++ 06_1차중간점검/main.py
@@ -1,1 +1,97 @@
 
+#1
+print('\'python\'은 프로그래밍 언어입니다.')
+
+#2
+# 화씨 온도입니다. 섭씨 온도는 0도 입니다.
+temperature1 = 32
+
+# 섭씨 온도입니다. 화씨 온도는 89.6도 입니다.
+temperature2 = 32
+
+convert1 = (temperature1 - 32) / 1.8
+convert2 = (temperature2 * 1.8) + 32
+
+print('화씨 {}도는 섭씨 {}도'.format(temperature1, convert1))
+print('섭씨 {}도는 화씨 {}도'.format(temperature2, convert2))
+
+#3
+year = 2023
+
+#4
+print(''Hello world!'')
+print(""Hello world!"")
+
+#5
+자 
+0과 문자 '10'은 다르다.
+
+#6
+s = '공격받은 몬스터는 {}의 데미지를 받았습니다.'
+print(s.format(10))
+print(s.format(50))
+print(s.format(297))
+
+#7
+age = int(input('당신의 나이는?'))
+year = 2024 - age
+print('당신의 올해 나이는 {}살 입니다. 당신은 {}년에 태어났습니다.'.format(age, year))
+
+#8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
