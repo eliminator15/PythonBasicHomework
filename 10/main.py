3f637e08c7f15a589d2936e7361052961eb5872a
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-03-02 17:00:24 +0900

    Update main.py

--- 10/main.py
+++ 10/main.py
@@ -1,3 +1,4 @@
+# 1
 scores = [0] * 5
 
 for i in range(5):
@@ -10,3 +11,16 @@ for i in range(5):
     
    @@@
         print('{}번 학생 불합격'.format(i+1))  
+
+# 2
+
+scores = [0] * 5
+
+for i in range(5):
+    scores[i] = int(input('{}번 학생의 성적을 입력하세요: '.format(i+1)))
+
+for i in range(5):
+    if scores[i] >= 60:
+        print('{}번 학생 합격'.format(i+1))
+    else:
+        print('{}번 학생 불합격'.format(i+1)) 
