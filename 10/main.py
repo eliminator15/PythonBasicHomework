28b826dc40c86d4222832e161bf66fe53a24cbe7
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-03-02 16:19:38 +0900

    Create main.py

---
+++ 10/main.py
@@ -0,0 +1,12 @@
+scores = [0] * 5
+
+for i in range(5):
+    scores[i] = int(input('{}번 학생의 성적 입력: '.format(i+1)))
+
+print('--- 입력된 값 ---')
+
+for i in range(5):
+    print('{}번 학생의 성적: {}'.format(i+1, score))
+    
+   @@@
+        print('{}번 학생 불합격'.format(i+1))  
