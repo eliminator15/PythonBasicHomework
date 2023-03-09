fd7ee869a2746d54361c8bed38315b1697acaac8
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-03-09 16:58:37 +0900

    Update main.py

--- 11/main.py
+++ 11/main.py
@@ -2,46 +2,10 @@
 dic1, dic8
 
 #2
+3, 6, 100
 2023, 2, 1, 3, 5, 10, 100, 283, 4, 6, 22, 302, 2022
-
 2023, 2, 1, 5, 10, 283
-4, 22, 302, 2022def print_command():
-    print('''1. 아이스크림 추가
-2. 아이스크림 가격 수정
-3. 아이스크림 삭제
-4. 아이스크림 출력
-5. 종료''')
-
-def print_product(product):
-    print('*'*50)
-    for key in product.keys():
-      print('{} : {}'.format(@@@, @@@))
-    print('*'*50)
-
-product = {'메로나': 1000,
-       '비비빅': 1200,
-       '빵빠레': 1800,
-       '죠스바': 1200,
-       '월드콘': 1500}
-
-while True:
-  print_command()
-  command = input('작업을 선택하세요. ')
-  if command == '1':
-      ice_cream = input('추가할 아이스크림 : ')
-      price = input('가격 : ')
-      @@@ # 아이스크림 추가
-  elif command == '2':
-      ice_cream = input('수정할 아이스크림 : ')
-      price = input('가격 : ')
-      @@@ # 아이스크림 수정
-  elif command == '3':
-      ice_cream = input('삭제할 아이스크림 : ')
-      @@@ # 아이스크림 삭제
-  elif command == '4':
-      @@@ # print_product 함수 호출
-  elif command == '5':
-      break # 멈춤
+4, 22, 302, 2022
 
 def print_command():
     print('''1. 아이스크림 추가
@@ -53,7 +17,7 @@ def print_command():
 def print_product(product):
     print('*'*50)
     for key in product.keys():
-      print('{} : {}'.format(@@@, @@@))
+        print('{} : {}'.format(key, product[key] ))
     print('*'*50)
 
 product = {'메로나': 1000,
@@ -66,17 +30,17 @@ while True:
   print_command()
   command = input('작업을 선택하세요. ')
   if command == '1':
-      ice_cream = input('추가할 아이스크림 : ')
-      price = input('가격 : ')
-      @@@ # 아이스크림 추가
+        ice_cream = input('추가할 아이스크림 : ')
+        price = input('가격 : ')
+        product[ice_cream] = price # 아이스크림 추가
   elif command == '2':
-      ice_cream = input('수정할 아이스크림 : ')
-      price = input('가격 : ')
-      @@@ # 아이스크림 수정
+        ice_cream = input('수정할 아이스크림 : ')
+        price = input('가격 : ')
+        product[ice_cream] = price # 아이스크림 수정
   elif command == '3':
-      ice_cream = input('삭제할 아이스크림 : ')
-      @@@ # 아이스크림 삭제
+        ice_cream = input('삭제할 아이스크림 : ')
+        del product[ice_cream]  # 아이스크림 삭제
   elif command == '4':
-      @@@ # print_product 함수 호출
+        print_product(product) # print_product 함수 호출
   elif command == '5':
-      @@@ # 멈춤
+        break # 멈춤
