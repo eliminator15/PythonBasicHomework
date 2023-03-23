34a0ace04f623f945e0991b949d06ce3878e2180
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-03-23 16:53:33 +0900

    Update main.py

--- 13/main.py
+++ 13/main.py
@@ -1,13 +1,24 @@
+import random
 
+class Pokemon:
+  def __init__(self, name, gender, age, hp):
+    self.name = name
+    self.gender = gender
+    self.age = age
+    self.hp = hp
 
-class Pikachu:
-    def __init__(self, 이름, 성별, 체력):
-        self.이름 = 이름
-        self.성별 = 성별
-        self.체력 = 체력
+  def cry(self):
+    print('낑')
+
+  def walk(self):
+    print('포켓몬이 걷는다.')
+
+class Pikachu(Pokemon):
+    def __init__(self, name, gender, age, hp):
+        super().__init__(name, gender, age, hp)
     
-    def 백만볼트(self):
-        print('{}의 백만볼트!'.format(self.이름))
+    def 볼트태클(self):
+        print('{}의 볼트태클!'.format(self.name))
         num = random.randint(1, 1000)
         if num <= 750:
             print('효과는 굉장했다!')
@@ -15,18 +26,16 @@ class Pikachu:
             print('효과는 괜찮았다.')
         else:
             print('효과는 미미했다.')
-    
+
+  
     def __str__(self):
-        sentence = '이름: {}, 성별: {}, 체력: {}'.format(self.이름, self.성별, self.체력)
+        sentence = '이름: {}, 성별: {}, 나이: {}, 체력: {}'.format(self.name, self.gender, self.age, self.hp)
         return sentence
 
-import random
-
-class Eevee:
-    def __init__(self, name, age, hp):
-        self.name = name
-        self.age = age
-        self.hp = hp
+class Eevee(Pokemon):
+    
+    def __init__(self, name, gender, age, hp):
+        super().__init__(name, gender, age, hp)
     
     def 이판사판태클(self):
         print('{}의 이판사판태클!'.format(self.name))
@@ -39,5 +48,7 @@ class Eevee:
             print('효과는 미미했다.') 
     
     def __str__(self):
-        sentence = '이름: {}, 나이: {}살, 체력: {}'.format(self.name, self.age, self.hp)
+        sentence = '이름: {}, 성별: {}, 나이: {}살, 체력: {}'.format(self.name, self.gender, self.age, self.hp)
         return sentence
+
+
