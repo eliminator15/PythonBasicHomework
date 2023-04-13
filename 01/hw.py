cfb6fe8c15cdf9beaf143d940161c43e8b2fdb2e
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-04-13 17:05:44 +0900

    Create hw.py

---
+++ 01/hw.py
@@ -0,0 +1,18 @@
+print('/'python/'은 프로그래밍 언어입니다.')
+11
+15
+25
+5
+27
+3
+7
+-13
+price1=1000
+price2=96900
+print(price1*0.05)
+print(price2*0.05)
+num3=472*5
+num4=472*8
+num5=472*3
+num6=num3+num4*10+num5*100
+print(num3, num4, num5, num6) 
