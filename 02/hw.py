cd227e57b8f1ecb42c040546e33f734cbd67a0f6
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-04-13 17:06:25 +0900

    Create hw.py

---
+++ 02/hw.py
@@ -0,0 +1,22 @@
+year=2011-1
+age=2022-2011
+print(age)
+
+def ready_for_school
+    print(일어나기)
+    print(밥 먹기)
+    print(세수하고 양치하기)
+    print(옷 입고 가방 싸기)
+    print(출발하기)
+ready_for_school()
+
+def go_school
+    print(출발하기)
+    print(행단보도 지나기)
+    print(정문 넘기)
+    print(교실 도착하기)
+ready_for_school()
+go_school()
+
+def smaile(진평 얼굴 송중기)
+smaile(진평 얼굴 송중기)()
